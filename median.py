4d9cb802ccae2308bd10f5201e6d9bbb60d381e9
Shijia Xu <xushijia0910@outlook.com>
2022-11-24 19:50:06 +0000

    finished

--- median.py
+++ median.py
@@ -1,6 +1,12 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_odd(int):
+    if int % 2 != 0:
+        return True
+    else:
+        return False
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +15,9 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+median = 0
+if is_odd(len(numbers)):
+    median = numbers[int((len(numbers)+1)/2-1)]
+else:
+    median = (numbers[int(len(numbers)/2-1)] + numbers[int(len(numbers)/2)])/2
+print(median)
